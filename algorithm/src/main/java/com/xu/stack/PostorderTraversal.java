2fff29b4a28ba9974dee96f4e08bad77f7ffc8ab
xuwei <xuwei@163.com>
2020-09-20 17:58:00 +0800

    算法

---
+++ algorithm/src/main/java/com/xu/stack/PostorderTraversal.java
@@ -0,0 +1,31 @@
+package com.xu.stack;
+
+import com.xu.baseStruct.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 145. 二叉树的后序遍历
+ **/
+public class PostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> list = new LinkedList<>();
+        if (root == null) {
+            return list;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            list.addFirst(node.val);
+            if (node.left != null) stack.push(node.left);
+            if(node.right != null) stack.push(node.right);
+        }
+        return list;
+    }
+}
